b6dab700be56129fd33e92a4ae0ce33d5b9726ae
tahyd <ta.hyd.003@gmail.com>
2024-05-27 21:20:40 +0530

    first

---
+++ bc1/src/main/java/com/example/demo/Bc1Application.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Bc1Application {
+
+	@GetMapping("/hello")
+	public String test() {
+		return "Hello World !";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Bc1Application.class, args);
+	}
+
+}
